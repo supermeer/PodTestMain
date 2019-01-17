aa2ff9b341cee1d4907199b2eb238112268e525c
曹永超 <caoyongchao@huitu.com>
2019-01-17 14:42:23 +0800

    第一次提交

---
+++ PodTestMain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTestMain
+//
+//  Created by 曹永超 on 2019/1/17.
+//  Copyright © 2019 曹永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTestMain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTestMain
+//
+//  Created by 曹永超 on 2019/1/17.
+//  Copyright © 2019 曹永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
